7415155c626b4518d642d51db43bec97c22235fb
Sergiy <toxa@mac-mini-toxa.local>
2019-04-30 20:19:49 +0300

    Initial Commit

---
+++ Wine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wine
+//
+//  Created by toxa on 4/30/19.
+//  Copyright © 2019 gbsoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
